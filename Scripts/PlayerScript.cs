2ce447c28c537ef2ac60c80ae99209006223b07a
ZathS2 <mateusmrm.mrm@gmail.com>
2025-01-11 23:27:37 -0300

    Só printar pra ver se da certo

--- Scripts/PlayerScript.cs
+++ Scripts/PlayerScript.cs
@@ -19,7 +19,7 @@ public partial class PlayerScript : Node2D
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
-	{	
+	{
 		//scene1 = (PackedScene) GD.Load<PackedScene>("res://Scenes//animal1.tscn");
 		//scene2 = (PackedScene) GD.Load<PackedScene>("res://Scenes//animal2.tscn");
 
@@ -28,8 +28,10 @@ public partial class PlayerScript : Node2D
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
-	{	
-		
+	{
+		var GameManager = (GodotObject)GetNode<Node>("/root/GameManager");
+
+		GD.Print(GameManager.Get("is_player_in_water"));
 	}
 
 	void switchAnimal(ANIMALS animal)
@@ -38,65 +40,62 @@ public partial class PlayerScript : Node2D
 			return;
 
 		currentAnimal = animal;
-		
+
 		switch (currentAnimal)
 		{
 			case (ANIMALS.ANIMAL1):
 
-				if(currentInstantiatedScene != null)
+				if (currentInstantiatedScene != null)
 				{
 					GlobalPosition = currentInstantiatedScene.GlobalPosition;
 					RemoveChild(currentInstantiatedScene);
 				}
 
-				
-				currentInstantiatedScene = (Node2D) scene1.Instantiate();
-				
+
+				currentInstantiatedScene = (Node2D)scene1.Instantiate();
+
 				AddChild(currentInstantiatedScene);
 
-				
+
 
 				break;
 
 			case (ANIMALS.ANIMAL2):
-				
-				if(currentInstantiatedScene != null)
+
+				if (currentInstantiatedScene != null)
 				{
 					GlobalPosition = currentInstantiatedScene.GlobalPosition;
 					RemoveChild(currentInstantiatedScene);
 				}
 
-				currentInstantiatedScene = (Node2D) scene2.Instantiate();
+				currentInstantiatedScene = (Node2D)scene2.Instantiate();
 
 				AddChild(currentInstantiatedScene);
 
-				
 
-				
+
+
 				break;
 		}
 	}
 
-	public override void _Input(InputEvent @event) {
+	public override void _Input(InputEvent @event)
+	{
 		if (@event is InputEventKey keyEvent && keyEvent.Pressed)
 		{
 			if (keyEvent.Keycode == Key.A)
 			{
 				switchAnimal(ANIMALS.ANIMAL1);
-				
+
 			}
 
 			if (keyEvent.Keycode == Key.S)
 			{
 				switchAnimal(ANIMALS.ANIMAL2);
-				
+
 			}
 		}
 		base._Input(@event);
 	}
 
-	private void OnWaterEnter()
-	{
-		GD.Print("NA AGUA");
-	}
 }
