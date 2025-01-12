650a3efd6cd91df8f01493ac51f2304f409614e8
Sophia Pardinho <pardinhosophia05@gmail.com>
2025-01-12 12:26:46 -0300

    Limpando parte que nn tava funcionando

---
+++ game_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var is_player_in_water
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(is_player_in_water)
+	pass
