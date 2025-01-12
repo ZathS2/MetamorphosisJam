334c2d4938a184ef88a0a31cb6cad3adf7a276b5
Sophia Pardinho <pardinhosophia05@gmail.com>
2025-01-12 12:23:54 -0300

    ADD Water Tile 2

---
+++ water.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+func _ready():
+	pass
+	
+func _process(delta):
+	var is_player_in_water = false
+	for body in get_overlapping_bodies():
+		if body.is_in_group("Player"):
+			is_player_in_water = true
+			break
+	GameManager.is_player_in_water = is_player_in_water
