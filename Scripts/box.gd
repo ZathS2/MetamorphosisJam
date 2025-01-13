790f5a2f0a9801b0309bb4393c39a6a2ac055381
Sophia Pardinho <pardinhosophia05@gmail.com>
2025-01-13 10:01:20 -0300

    Bordinha da colisão arrumada

--- Scripts/box.gd
+++ Scripts/box.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	lock_rotation = true
 	pass # Replace with function body.
 
 
@@ -11,9 +12,9 @@ func _process(delta):
 	for body in $Area2D.get_overlapping_bodies():
 		if body.is_in_group("Player"):
 			var player_position = body.global_position
-			if Vector2(global_position-player_position).x>0:
-				apply_central_impulse(Vector2(-25, 0))
-			elif Vector2(global_position-player_position).x<0:
-				apply_central_impulse(Vector2(25, 0))
-				pass
+			if Vector2(global_position-player_position).y<90:
+				if Vector2(global_position-player_position).x>0:
+					apply_central_impulse(Vector2(25, 0))
+				elif Vector2(global_position-player_position).x<0:
+					apply_central_impulse(Vector2(-25, 0))
 	pass
