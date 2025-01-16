9af82be9730db607f49e40237b79216bcb7face5
Sophia Pardinho <pardinhosophia05@gmail.com>
2025-01-16 00:19:56 -0300

    Algo ocorreu KKKKKKKKKK

--- Scripts/Objects/box.gd
+++ Scripts/Objects/box.gd
@@ -12,11 +12,22 @@ func _ready():
 func _process(delta):
 	if !beeing_caried:
 		for body in $Area2D.get_overlapping_bodies():
-			if body.is_in_group("Player"):
-				var player_position = body.global_position
-				if Vector2(global_position-player_position).y<90:
-					if Vector2(global_position-player_position).x>0:
-						apply_central_impulse(Vector2(25, 0))
-					elif Vector2(global_position-player_position).x<0:
-						apply_central_impulse(Vector2(-25, 0))
+			if get_groups().has("Catchable"):
+				if body.is_in_group("Player"):
+					print(body.global_position-global_position)
+					var player_position = body.global_position
+					if Vector2(global_position-player_position).y<60:
+						if Vector2(global_position-player_position).x>0:
+							apply_central_impulse(Vector2(25, 0))
+						elif Vector2(global_position-player_position).x<0:
+							apply_central_impulse(Vector2(-25, 0))
+			else:
+				if body.is_in_group("Onça"):
+					print(body.global_position-global_position)
+					var player_position = body.global_position
+					if Vector2(global_position-player_position).y<90:
+						if Vector2(global_position-player_position).x>0:
+							apply_central_impulse(Vector2(25, 0))
+						elif Vector2(global_position-player_position).x<0:
+							apply_central_impulse(Vector2(-25, 0))
 		pass
--- Scripts/World/platform.gd
+++ Scripts/World/platform.gd
@@ -1,17 +1,19 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 @export var final_position_marker: Marker2D
 @export var speed: float = 100
 @export var leverscript : Node
 
-@onready var final_position = final_position_marker.global_position
+@onready var final_position
 
 var inicial_position
 var is_button_pressed = false
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	freeze_mode=1
+	if final_position_marker:
+		final_position = final_position_marker.global_position
 	if final_position == null:
 		final_position = global_position
 	inicial_position = global_position
@@ -24,18 +26,24 @@ func _ready():
 func _process(delta):
 	if is_button_pressed:
 		if global_position!=final_position:
-			velocity = speed * (final_position - global_position).normalized()
-			move_and_slide()
-		elif velocity!=Vector2(0, 0):
-			velocity = Vector2(0, 0)
-			move_and_slide()
+			if freeze:
+				freeze=false
+			apply_central_impulse(speed * (final_position - global_position).normalized())
+			for body in $Area2D.get_overlapping_bodies():
+				if body!=self:
+					body.global_position+=Vector2(0,5)
+		elif !freeze:
+			freeze=true
 	else:
 		if global_position!=inicial_position:
-			velocity = speed * (inicial_position - global_position).normalized()
-			move_and_slide()
-		elif velocity!=Vector2(0, 0):
-			velocity = Vector2(0, 0)
-			move_and_slide()
+			if freeze:
+				freeze=false
+			apply_central_impulse(speed * (inicial_position - global_position).normalized())
+			for body in $Area2D.get_overlapping_bodies():
+				if body!=self:
+					body.global_position+=Vector2(0,5)
+		elif !freeze:
+			freeze=true
 	pass
 '''
 func _move_plataform_lever():
