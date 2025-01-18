0759020190bfc0aa20d031b30b5c638b07d067e9
ZathS2 <mateusmrm.mrm@gmail.com>
2025-01-18 01:04:51 -0300

    onca

--- Scripts/Objects/box.gd
+++ Scripts/Objects/box.gd
@@ -3,10 +3,13 @@ extends RigidBody2D
 var beeing_caried = false
 var being_pushed = false
 
+var pushingDir : int = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	lock_rotation = true
 	#can_sleep = false
+	
 	pass # Replace with function body.
 
 
@@ -17,31 +20,39 @@ func _process(delta):
 		for body in $Area2D.get_overlapping_bodies():
 			if get_groups().has("Catchable"):
 				if body.is_in_group("Player"):
+					var speed = 150
+					if body.is_in_group("Onça"):
+						speed = 250
+					
 					being_pushed = true
 					var player_position = body.global_position
 					if Vector2(global_position-player_position).y<60:
 						if Vector2(global_position-player_position).x>0:
-							if linear_velocity.length()<100:
+							if linear_velocity.length()<speed:
 								apply_central_impulse(Vector2(25, 0))
+								pushingDir = 1
 						elif Vector2(global_position-player_position).x<0:
-							if linear_velocity.length()<100:
+							if linear_velocity.length()<speed:
 								apply_central_impulse(Vector2(-25, 0))
+								pushingDir = -1
 			else:
 				if body.is_in_group("Onça"):
 					being_pushed = true
 					var player_position = body.global_position
 					if Vector2(global_position-player_position).y<90:
 						if Vector2(global_position-player_position).x>0:
-							if linear_velocity.length()<100:
+							if linear_velocity.length()<200:
 								apply_central_impulse(Vector2(25, 0))
+								pushingDir = 1
 						elif Vector2(global_position-player_position).x<0:
-							if linear_velocity.length()<100:
+							if linear_velocity.length()<200:
 								apply_central_impulse(Vector2(-25, 0))
+								pushingDir = -1
 	
 	if being_pushed==false:
-		print(linear_velocity.length())
-		if round(abs(linear_velocity.x))>0:
-			apply_central_impulse(Vector2(sign(linear_velocity.x)*-1, 0))
+		if !is_zero_approx(linear_velocity.x): 
+			apply_central_impulse((-linear_velocity.x / 2 * mass) * Vector2.RIGHT)
+			 
 	
 	var ray_cast_collider_down = $RayCastDown.get_collider()
 	if ray_cast_collider_down!= null:
