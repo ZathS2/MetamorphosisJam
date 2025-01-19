47213a1e2a61bdf96f34a021dc89958f9f9c9ae5
ZathS2 <mateusmrm.mrm@gmail.com>
2025-01-19 00:19:49 -0300

    efeito sonoro de transição

--- Scripts/ScenesHandler/SceneArea.cs
+++ Scripts/ScenesHandler/SceneArea.cs
@@ -4,6 +4,9 @@ using System;
 public partial class SceneArea : Node
 {
 	[Export] PackedScene nextScene;
+
+	[Export] AudioStreamPlayer audio;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -15,12 +18,20 @@ public partial class SceneArea : Node
 	}
 
 
-	public void GoToNextScene(Node2D body)
+	public async void GoToNextScene(Node2D body)
 	{
 		if (!body.IsInGroup("Player"))
 			return;
-			
+
+		if (audio.Playing == false)
+			audio.Play();
+
+		GD.Print("VAI TOMA NO CU " + audio.Playing);
+
+		await ToSignal(audio, "finished");
+
 		var sceneHandler = (MainSceneHandler)GetNode<Node>("/root/MainSceneHandler");
 		sceneHandler.ChangeSceneTo(nextScene);
+
 	}
 }
